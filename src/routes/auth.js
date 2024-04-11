408769ffd66fe3d1235588de250b951e6918bd04
Marty Rabatel <marty.rabatel2@gmail.com>
2024-04-11 10:11:16 +0200

    Ajout verification des infos avant d'injecter en base avec express validator

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,11 +1,14 @@
 import express from "express";
 import { signup, signin } from "../controller/authController.js";
-
-// import { body } from 'express-validator';
+import { body } from "express-validator";
 
 const router = express.Router();
 
 router.post("/signup", signup);
-router.post("/signin", signin);
+router.post(
+  "/signin",
+  [body("email").isEmail(), body("desc").trim().isLength({ min: 5, max: 100 })],
+  signin
+);
 
 export default router;
